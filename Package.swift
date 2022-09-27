73e3f948005349d41d987f182228962288f38087
Jonah Pelfrey <pelfreyjonah@gmail.com>
2022-09-27 11:45:39 -0500

    declare resources in package

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VaultSwiftTheme",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Fonts")]
+        ),
         .testTarget(
             name: "VaultSwiftThemeTests",
             dependencies: ["VaultSwiftTheme"]),
