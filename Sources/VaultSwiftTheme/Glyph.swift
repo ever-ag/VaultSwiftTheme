385937fddaa650c79c4d10f65ea6c051ed86cebd
Jonah Pelfrey <pelfreyjonah@gmail.com>
2022-10-18 13:36:00 -0500

    Add glyphs

---
+++ Sources/VaultSwiftTheme/Glyph.swift
@@ -0,0 +1,14 @@
+//
+//  Glyph.swift
+//  
+//
+//  Created by Jonah Pelfrey on 10/18/22.
+//
+
+import Foundation
+
+final class Glyph {
+    static let arrowUp: String = "↗"
+    static let arrowDown: String = "↙"
+    static let bullet: String = "∙"
+}
